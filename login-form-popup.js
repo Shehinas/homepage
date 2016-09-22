1302089f8a24da015a3c8d17e0995ab87124265a
Shehinas <shehinas@qburst.com>
2016-09-22 17:28:26 +0530

    implement the login form-add form validationd-the header shows logout button and hide login button-show hi user

--- login-form-popup.js
+++ login-form-popup.js
@@ -1,12 +1,20 @@
 function formValidation() {
-        var userNameValue = document.getElementById("username").value,
-        	passwordValue = document.getElementById("username").value,
-        	userMsg = document.getElementById("user");
-        
-        if (userNameValue.trim().length>0 && passwordValue.trim().length>0 ) {
-            userMsg.innerHTML = "Hi "+userNameValue;
-        } else {
-        	alert("Please enter Username/Password to login!");
-        }
-        
+    var userNameValue = document.getElementById("username").value,
+        passwordValue = document.getElementById("password").value,
+        userMsg = document.getElementById("user"),
+        logoutBtn = document.getElementById('logout-btn'),
+        loginBtn = document.getElementById('login-btn');
+
+    if (userNameValue.trim().length > 0 && passwordValue.trim().length > 0) {
+        userMsg.innerHTML = "Hi " + userNameValue;
+        window.location.hash = "";
+        logoutBtn.classList.remove('hide');
+        logoutBtn.classList.add('show-inline-block');
+        if (loginBtn.classList.contains('show-inline-block')) {
+        	loginBtn.classList.remove('show-inline-block');
+        }        
+        loginBtn.classList.add('hide');
+    } else {
+        alert("Please enter Username/Password to login!");
+    }
 }
