3d979b2120f8ea6678d0c232685c0fae6f68b1f8
Shehinas <shehinas@qburst.com>
2016-09-23 11:59:16 +0530

    change image reference,ammend logout button

--- login-form-popup.js
+++ login-form-popup.js
@@ -2,16 +2,16 @@ function formValidation() {
     var userNameValue = document.getElementById("username").value,
         passwordValue = document.getElementById("password").value,
         userMsg = document.getElementById("user"),
-        logoutBtn = document.getElementById('logout-btn'),
-        loginBtn = document.getElementById('login-btn');
+        logoutBtn = document.getElementById('logoutBtn'),
+        loginBtn = document.getElementById('loginBtn');
 
     if (userNameValue.trim().length > 0 && passwordValue.trim().length > 0) {
         userMsg.innerHTML = "Hi " + userNameValue;
         window.location.hash = "";
         logoutBtn.classList.remove('hide');
-        logoutBtn.classList.add('show-inline-block');
-        if (loginBtn.classList.contains('show-inline-block')) {
-        	loginBtn.classList.remove('show-inline-block');
+        logoutBtn.classList.add('show_inline_block');
+        if (loginBtn.classList.contains('show_inline_block')) {
+        	loginBtn.classList.remove('show_inline_block');
         }        
         loginBtn.classList.add('hide');
     } else {
